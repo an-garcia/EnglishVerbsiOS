f4d0a6a20c63f87d919e5bff227fd849a6f8fcdd
Angel Newton <xanewton@gmail.com>
2017-12-14 08:14:27 -0500

    Add Conjugation to Model

---
+++ EnglishVerbsiOS/Conjugation.swift
@@ -0,0 +1,94 @@
+//
+//  Conjugation.swift
+//  EnglishVerbsiOS
+//
+//  Created by xengar on 2017-12-14.
+//  Copyright © 2017 xengar. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Conjugation
+class Conjugation {
+
+    // MARK: Properties
+    var simplePresentI : String = ""
+    var simplePresentYou : String = ""
+    var simplePresentHe : String = ""
+    var simplePresentWe : String = ""
+    var simplePresentYoup : String = ""
+    var simplePresentThey : String = ""
+    var presentContinuousI : String = ""
+    var presentContinuousYou : String = ""
+    var presentContinuousHe : String = ""
+    var presentContinuousWe : String = ""
+    var presentContinuousYoup : String = ""
+    var presentContinuousThey : String = ""
+    var presentPerfectI : String = ""
+    var presentPerfectYou : String = ""
+    var presentPerfectHe : String = ""
+    var presentPerfectWe : String = ""
+    var presentPerfectYoup : String = ""
+    var presentPerfectThey : String = ""
+    var presentPerfectContinuousI : String = ""
+    var presentPerfectContinuousYou : String = ""
+    var presentPerfectContinuousHe : String = ""
+    var presentPerfectContinuousWe : String = ""
+    var presentPerfectContinuousYoup : String = ""
+    var presentPerfectContinuousThey : String = ""
+    
+    var simplePastI : String = ""
+    var simplePastYou : String = ""
+    var simplePastHe : String = ""
+    var simplePastWe : String = ""
+    var simplePastYoup : String = ""
+    var simplePastThey : String = ""
+    var pastContinuousI : String = ""
+    var pastContinuousYou : String = ""
+    var pastContinuousHe : String = ""
+    var pastContinuousWe : String = ""
+    var pastContinuousYoup : String = ""
+    var pastContinuousThey : String = ""
+    var pastPerfectI : String = ""
+    var pastPerfectYou : String = ""
+    var pastPerfectHe : String = ""
+    var pastPerfectWe : String = ""
+    var pastPerfectYoup : String = ""
+    var pastPerfectThey : String = ""
+    var pastPerfectContinuousI : String = ""
+    var pastPerfectContinuousYou : String = ""
+    var pastPerfectContinuousHe : String = ""
+    var pastPerfectContinuousWe : String = ""
+    var pastPerfectContinuousYoup : String = ""
+    var pastPerfectContinuousThey : String = ""
+    
+    var simpleFutureI : String = ""
+    var simpleFutureYou : String = ""
+    var simpleFutureHe : String = ""
+    var simpleFutureWe : String = ""
+    var simpleFutureYoup : String = ""
+    var simpleFutureThey : String = ""
+    var futureContinuousI : String = ""
+    var futureContinuousYou : String = ""
+    var futureContinuousHe : String = ""
+    var futureContinuousWe : String = ""
+    var futureContinuousYoup : String = ""
+    var futureContinuousThey : String = ""
+    var futurePerfectI : String = ""
+    var futurePerfectYou : String = ""
+    var futurePerfectHe : String = ""
+    var futurePerfectWe : String = ""
+    var futurePerfectYoup : String = ""
+    var futurePerfectThey : String = ""
+    var futurePerfectContinuousI : String = ""
+    var futurePerfectContinuousYou : String = ""
+    var futurePerfectContinuousHe : String = ""
+    var futurePerfectContinuousWe : String = ""
+    var futurePerfectContinuousYoup : String = ""
+    var futurePerfectContinuousThey : String = ""
+    
+    // MARK: Initializer
+    init() {
+    }
+    
+}
--- EnglishVerbsiOS/VerbData.swift
+++ EnglishVerbsiOS/VerbData.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-// MARK: - Verb
+// MARK: - VerbData
 class VerbData {
 
     // MARK: Properties
